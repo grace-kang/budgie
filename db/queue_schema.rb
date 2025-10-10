c76a20a3f72ac58840cae5fadbffb1ff50804fef
Grace Kang <gracek316@gmail.com>
2025-10-10 12:28:28 -0700

    Install and run `prettier`
    Global isn't good enough since we need to include the prettier ERB
    plugin.

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -8,24 +8,34 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.string 'concurrency_key', null: false
     t.datetime 'expires_at', null: false
     t.datetime 'created_at', null: false
-    t.index %w[concurrency_key priority job_id], name: 'index_solid_queue_blocked_executions_for_release'
-    t.index %w[expires_at concurrency_key], name: 'index_solid_queue_blocked_executions_for_maintenance'
-    t.index ['job_id'], name: 'index_solid_queue_blocked_executions_on_job_id', unique: true
+    t.index %w[concurrency_key priority job_id],
+            name: 'index_solid_queue_blocked_executions_for_release'
+    t.index %w[expires_at concurrency_key],
+            name: 'index_solid_queue_blocked_executions_for_maintenance'
+    t.index ['job_id'],
+            name: 'index_solid_queue_blocked_executions_on_job_id',
+            unique: true
   end
 
   create_table 'solid_queue_claimed_executions', force: :cascade do |t|
     t.bigint 'job_id', null: false
     t.bigint 'process_id'
     t.datetime 'created_at', null: false
-    t.index ['job_id'], name: 'index_solid_queue_claimed_executions_on_job_id', unique: true
-    t.index %w[process_id job_id], name: 'index_solid_queue_claimed_executions_on_process_id_and_job_id'
+    t.index ['job_id'],
+            name: 'index_solid_queue_claimed_executions_on_job_id',
+            unique: true
+    t.index %w[process_id job_id],
+            name:
+              'index_solid_queue_claimed_executions_on_process_id_and_job_id'
   end
 
   create_table 'solid_queue_failed_executions', force: :cascade do |t|
     t.bigint 'job_id', null: false
     t.text 'error'
     t.datetime 'created_at', null: false
-    t.index ['job_id'], name: 'index_solid_queue_failed_executions_on_job_id', unique: true
+    t.index ['job_id'],
+            name: 'index_solid_queue_failed_executions_on_job_id',
+            unique: true
   end
 
   create_table 'solid_queue_jobs', force: :cascade do |t|
@@ -42,14 +52,18 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.index ['active_job_id'], name: 'index_solid_queue_jobs_on_active_job_id'
     t.index ['class_name'], name: 'index_solid_queue_jobs_on_class_name'
     t.index ['finished_at'], name: 'index_solid_queue_jobs_on_finished_at'
-    t.index %w[queue_name finished_at], name: 'index_solid_queue_jobs_for_filtering'
-    t.index %w[scheduled_at finished_at], name: 'index_solid_queue_jobs_for_alerting'
+    t.index %w[queue_name finished_at],
+            name: 'index_solid_queue_jobs_for_filtering'
+    t.index %w[scheduled_at finished_at],
+            name: 'index_solid_queue_jobs_for_alerting'
   end
 
   create_table 'solid_queue_pauses', force: :cascade do |t|
     t.string 'queue_name', null: false
     t.datetime 'created_at', null: false
-    t.index ['queue_name'], name: 'index_solid_queue_pauses_on_queue_name', unique: true
+    t.index ['queue_name'],
+            name: 'index_solid_queue_pauses_on_queue_name',
+            unique: true
   end
 
   create_table 'solid_queue_processes', force: :cascade do |t|
@@ -61,9 +75,13 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.text 'metadata'
     t.datetime 'created_at', null: false
     t.string 'name', null: false
-    t.index ['last_heartbeat_at'], name: 'index_solid_queue_processes_on_last_heartbeat_at'
-    t.index %w[name supervisor_id], name: 'index_solid_queue_processes_on_name_and_supervisor_id', unique: true
-    t.index ['supervisor_id'], name: 'index_solid_queue_processes_on_supervisor_id'
+    t.index ['last_heartbeat_at'],
+            name: 'index_solid_queue_processes_on_last_heartbeat_at'
+    t.index %w[name supervisor_id],
+            name: 'index_solid_queue_processes_on_name_and_supervisor_id',
+            unique: true
+    t.index ['supervisor_id'],
+            name: 'index_solid_queue_processes_on_supervisor_id'
   end
 
   create_table 'solid_queue_ready_executions', force: :cascade do |t|
@@ -71,9 +89,12 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.string 'queue_name', null: false
     t.integer 'priority', default: 0, null: false
     t.datetime 'created_at', null: false
-    t.index ['job_id'], name: 'index_solid_queue_ready_executions_on_job_id', unique: true
+    t.index ['job_id'],
+            name: 'index_solid_queue_ready_executions_on_job_id',
+            unique: true
     t.index %w[priority job_id], name: 'index_solid_queue_poll_all'
-    t.index %w[queue_name priority job_id], name: 'index_solid_queue_poll_by_queue'
+    t.index %w[queue_name priority job_id],
+            name: 'index_solid_queue_poll_by_queue'
   end
 
   create_table 'solid_queue_recurring_executions', force: :cascade do |t|
@@ -81,9 +102,13 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.string 'task_key', null: false
     t.datetime 'run_at', null: false
     t.datetime 'created_at', null: false
-    t.index ['job_id'], name: 'index_solid_queue_recurring_executions_on_job_id', unique: true
-    t.index %w[task_key run_at], name: 'index_solid_queue_recurring_executions_on_task_key_and_run_at',
-                                 unique: true
+    t.index ['job_id'],
+            name: 'index_solid_queue_recurring_executions_on_job_id',
+            unique: true
+    t.index %w[task_key run_at],
+            name:
+              'index_solid_queue_recurring_executions_on_task_key_and_run_at',
+            unique: true
   end
 
   create_table 'solid_queue_recurring_tasks', force: :cascade do |t|
@@ -98,7 +123,9 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.text 'description'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['key'], name: 'index_solid_queue_recurring_tasks_on_key', unique: true
+    t.index ['key'],
+            name: 'index_solid_queue_recurring_tasks_on_key',
+            unique: true
     t.index ['static'], name: 'index_solid_queue_recurring_tasks_on_static'
   end
 
@@ -108,8 +135,11 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.integer 'priority', default: 0, null: false
     t.datetime 'scheduled_at', null: false
     t.datetime 'created_at', null: false
-    t.index ['job_id'], name: 'index_solid_queue_scheduled_executions_on_job_id', unique: true
-    t.index %w[scheduled_at priority job_id], name: 'index_solid_queue_dispatch_all'
+    t.index ['job_id'],
+            name: 'index_solid_queue_scheduled_executions_on_job_id',
+            unique: true
+    t.index %w[scheduled_at priority job_id],
+            name: 'index_solid_queue_dispatch_all'
   end
 
   create_table 'solid_queue_semaphores', force: :cascade do |t|
@@ -123,10 +153,28 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.index ['key'], name: 'index_solid_queue_semaphores_on_key', unique: true
   end
 
-  add_foreign_key 'solid_queue_blocked_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
-  add_foreign_key 'solid_queue_claimed_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
-  add_foreign_key 'solid_queue_failed_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
-  add_foreign_key 'solid_queue_ready_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
-  add_foreign_key 'solid_queue_recurring_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
-  add_foreign_key 'solid_queue_scheduled_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
+  add_foreign_key 'solid_queue_blocked_executions',
+                  'solid_queue_jobs',
+                  column: 'job_id',
+                  on_delete: :cascade
+  add_foreign_key 'solid_queue_claimed_executions',
+                  'solid_queue_jobs',
+                  column: 'job_id',
+                  on_delete: :cascade
+  add_foreign_key 'solid_queue_failed_executions',
+                  'solid_queue_jobs',
+                  column: 'job_id',
+                  on_delete: :cascade
+  add_foreign_key 'solid_queue_ready_executions',
+                  'solid_queue_jobs',
+                  column: 'job_id',
+                  on_delete: :cascade
+  add_foreign_key 'solid_queue_recurring_executions',
+                  'solid_queue_jobs',
+                  column: 'job_id',
+                  on_delete: :cascade
+  add_foreign_key 'solid_queue_scheduled_executions',
+                  'solid_queue_jobs',
+                  column: 'job_id',
+                  on_delete: :cascade
 end
