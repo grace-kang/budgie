53358de5aa960022cde42fcd2ac8603cee36aeaf
Grace Kang <gracek316@gmail.com>
2025-10-15 10:01:21 -0700

    Set budget background based on remaining amount
    Display green, red, or orange background depending on the budget
    remaining.

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Budget < ApplicationRecord
+  has_many :transactions, dependent: :destroy
+
   validates :name, presence: true
   validates :total, presence: true
 end
