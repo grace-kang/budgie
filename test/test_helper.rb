56f158089564cc12c8701cc8d1a6a4009777d0db
Grace Kang <gracek316@gmail.com>
2025-10-28 17:19:02 -0700

    Stub out all vite helpers for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,3 +15,16 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+module ViteHelpersStub
+  def vite_javascript_tag(*_args)
+    '' # return empty string instead of raising
+  end
+
+  def vite_client_tag(*_args)
+    ''
+  end
+end
+
+# Include in all views during tests
+ActiveSupport.on_load(:action_view) { include ViteHelpersStub }
