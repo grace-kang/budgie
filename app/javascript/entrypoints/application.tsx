774354fce523b3ae995308eefbabbe1956dd0869
Grace Kang <gracek316@gmail.com>
2025-10-28 17:06:19 -0700

    Run `yarn fixit`

--- app/javascript/entrypoints/application.tsx
+++ app/javascript/entrypoints/application.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
 if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-    </React.StrictMode>
-  )
+  ReactDOM.createRoot(root).render(<React.StrictMode></React.StrictMode>);
 }
