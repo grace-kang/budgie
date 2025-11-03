d1a6e986e48a8187b2a372932920da3ff6447688
Grace Kang <gracek316@gmail.com>
2025-11-02 19:18:03 -0800

    Add React Query to application entrypoint

--- app/javascript/entrypoints/application.tsx
+++ app/javascript/entrypoints/application.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../components/App';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from '../src/components/App';
 
 const root = document.getElementById('root');
 
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={new QueryClient()}>
+        <App />
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 }
