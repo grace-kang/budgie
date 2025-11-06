31822b3abcf61a71f75acd49fc2f11b20fe27964
Grace Kang <gracek316@gmail.com>
2025-11-06 11:58:18 -0800

    Require login for all endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,20 @@
 
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
+
+  before_action :require_login
   helper_method :current_user
+
+  private
+
+  def require_login
+    return if current_user
+
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: 'Please sign in' }
+      format.json { render json: { error: 'Unauthorized' }, status: :unauthorized }
+    end
+  end
 
   def current_user
     return @current_user if defined?(@current_user)
