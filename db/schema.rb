658f097eb9e370018c9cfa390c6f9509ad036784
Grace Kang <gracek316@gmail.com>
2025-11-06 09:33:06 -0800

    Add unique index to users email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_06_171410) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_06_172943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_06_171410) do
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "budgets", "months"
