b036dd6bac1df6a8f1850152fb5140cb3bf6b30d
Grace Kang <gracek316@gmail.com>
2026-01-09 19:20:57 -0800

    Add model and update dependencies

--- app/models/month.rb
+++ app/models/month.rb
@@ -3,6 +3,8 @@
 class Month < ApplicationRecord
   belongs_to :user
   has_many :transactions, dependent: :destroy
+  has_many :custom_budget_limits, dependent: :destroy
+  has_many :budgets, through: :custom_budget_limits
 
   validates :month, presence: true
   validates :year, presence: true
