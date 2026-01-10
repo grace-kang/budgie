cd440ef0f6f05d4fcb28e52a8b64979e4dab4103
Grace Kang <gracek316@gmail.com>
2026-01-09 16:22:42 -0800

    Remove create and destroy month
    We're no longer manually creating and deleting months so we can
    safely remove these.

--- test/controllers/months_controller_test.rb
+++ test/controllers/months_controller_test.rb
@@ -7,32 +7,4 @@ class MonthsControllerTest < ActionDispatch::IntegrationTest
     get months_url, headers: auth_headers
     assert_response :success
   end
-
-  test 'fails to create month with invalid data' do
-    assert_no_difference('Month.count') do
-      post months_url, headers: auth_headers, params: { month: { month: nil, year: 2024 } }
-    end
-    assert_response :unprocessable_entity
-  end
-
-  test 'should destroy month' do
-    month = test_user.months.create(month: 3, year: 2024)
-    assert_difference('Month.count', -1) do
-      delete month_url(month), headers: auth_headers
-    end
-    assert_response :ok
-  end
-
-  test 'fails to destroy non-existent month' do
-    assert_no_difference('Month.count') do
-      delete month_url(id: 'non-existent-id'), headers: auth_headers
-    end
-    assert_response :not_found
-  end
-
-  private
-
-  def test_user
-    User.create(email: 'test@example.com', provider: 'google', uid: '12345')
-  end
 end
